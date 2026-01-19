12091eb9e0bb87afccb6dab479f60b86a6e176ac
Mykola Totsevych <totsevich02@gmail.com>
2026-01-19 15:01:59 +0100

    add routes for managing own profile and retrieving other users' profiles

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from user.views import UserLoginView, UserCreateView, UserLogoutView
+from user.views import (
+    UserLoginView,
+    UserCreateView,
+    UserLogoutView,
+    UserManageYourProfileView,
+    UserRetrieveOtherProfileView
+)
 
 app_name = "user"
 
@@ -8,4 +14,14 @@ urlpatterns = [
     path("register/", UserCreateView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
+    path(
+        "profile/me/",
+        UserManageYourProfileView.as_view(),
+        name="manage-my-profile"
+    ),
+    path(
+        "profile/<int:pk>/",
+        UserRetrieveOtherProfileView.as_view(),
+        name="retrieve-other-profile"
+    ),
 ]
