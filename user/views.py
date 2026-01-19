31b2eb70f165d1755c3b3b31c4550f32fe3857e4
Mykola Totsevych <totsevich02@gmail.com>
2026-01-19 14:27:33 +0100

    add user registration and token-based login views

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from user.serializers import UserCreateSerializer, AuthTokenSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+    authentication_classes = ()
+    permission_classes = ()
+
+
+class UserLoginView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    authentication_classes = ()
+    renderer_classes = (JSONRenderer, BrowsableAPIRenderer,)
