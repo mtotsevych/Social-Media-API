bb5073fe297b3a1342c06e37630d5e91949b7cd4
Mykola Totsevych <totsevich02@gmail.com>
2026-01-19 14:31:15 +0100

    add logout route to user URLs

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import UserLoginView, UserCreateView
+from user.views import UserLoginView, UserCreateView, UserLogoutView
 
 app_name = "user"
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
 ]
