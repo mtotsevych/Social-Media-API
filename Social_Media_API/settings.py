53eb47b4a868e1029d507047e779bf9891dbfb85
Mykola Totsevych <totsevich02@gmail.com>
2026-01-19 14:21:47 +0100

    enable DRF, token auth, media config and custom user model

--- Social_Media_API/settings.py
+++ Social_Media_API/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "/media/"
+
+AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
