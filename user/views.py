ef2e3e02e8acabc2a664f4831cd9e3a5db6cc2f1
Mykola Totsevych <totsevich02@gmail.com>
2026-01-19 17:14:16 +0100

    add subscribe and unsubscribe endpoints

--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.db.models import QuerySet
 from rest_framework import generics, status
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.request import Request
@@ -61,13 +62,54 @@ class UserListView(generics.ListAPIView):
         return queryset
 
 
-class UserManageYourProfileView(generics.RetrieveUpdateAPIView):
+class UserYourProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserDetailSerializer
 
     def get_object(self) -> User:
         return self.request.user
 
 
-class UserRetrieveOtherProfileView(generics.RetrieveAPIView):
+class UserOtherProfileView(generics.RetrieveAPIView):
     serializer_class = UserDetailSerializer
     queryset = get_user_model().objects.all()
+
+
+class UserSubscribeView(APIView):
+    def post(self, request: Request, pk: int) -> Response:
+        user = get_object_or_404(get_user_model(), pk=pk)
+
+        if user == request.user:
+            return Response(
+                {"detail": "You cannot subscribe to yourself"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if request.user.subscriptions.filter(pk=pk).exists():
+            return Response(
+                {"detail": "Already subscribed"},
+                status=status.HTTP_200_OK
+            )
+
+        request.user.subscriptions.add(user)
+
+        return Response(
+            {"detail": f"You are now subscribed to {user.email}"},
+            status=status.HTTP_201_CREATED
+        )
+
+
+class UserUnsubscribeView(APIView):
+    def post(self, request: Request, pk: int) -> Response:
+        user = get_object_or_404(get_user_model(), pk=pk)
+
+        if request.user.subscriptions.filter(pk=pk).exists():
+            request.user.subscriptions.remove(user)
+            return Response(
+                {"detail": f"You are unsubscribed from {user.email}"},
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            {"detail": f"You are not subscribed to {user.email}"},
+            status=status.HTTP_200_OK
+        )
